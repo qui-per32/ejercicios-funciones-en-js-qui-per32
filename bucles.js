b0ccdbbb1bd1831b46c898a2c0117b66cd94970e
Juan <jupegarnica@gmail.com>
2018-05-11 19:27:04 +0200

    bucles y fizzbuzz added

---
+++ bucles.js
@@ -0,0 +1,128 @@
+
+
+
+// haz un bucle que logee los impares del 3 al 17 inclusive
+// Utilizad el bucle for
+
+
+// Logeea la tabla del 7;
+// Utilizad el bucle for
+
+
+let gente = [
+  {nombre:'Jamiro', edad:45},
+  {nombre: 'Juan', edad: 35},
+  {nombre: 'Paco', edad:34},
+  {nombre: 'Pepe', edad:14},
+  {nombre: 'Pilar', edad:24},
+  {nombre: 'Laura', edad:24},
+  {nombre: 'Jenny', edad:10},
+]
+
+// crea un array con la gente mayor de 25 años.
+// y muestralo por consola.
+// Sin utilizar el metodo filter de los arrays
+// Utilizad el bucle for of
+
+
+// crea un array con la gente que empieza por J
+// Sin utilizar el metodo filter de los arrays
+// Utilizad el bucle for of
+// y muestralo por consola.
+
+
+// crea un array con la gente que su nombre tiene 4 letras
+// utilizando el metodo filter de los arrays
+// y muestralo por consola.
+
+
+// crea un array con la gente que su nombre empieza por J o L
+// utilizando el metodo filter de los arrays
+// y muestralo por consola.
+
+
+
+// Rellena la matriz como la siguiente utilizando bucles for anidados
+// let matriz = [
+//   ['00','01','02'],
+//   ['10','11','12'],
+//   ['20','21','22']
+// ];
+let matriz = [
+  [],
+  [],
+  []
+]
+
+// haz un bucle que logee los impares del 3 al 17 inclusive
+// Utilizad el bucle for
+
+
+// Logeea la tabla del 7;
+// Utilizad el bucle for
+
+
+let gente = [{
+        nombre: 'Jamiro',
+        edad: 45
+    },
+    {
+        nombre: 'Juan',
+        edad: 35
+    },
+    {
+        nombre: 'Paco',
+        edad: 34
+    },
+    {
+        nombre: 'Pepe',
+        edad: 14
+    },
+    {
+        nombre: 'Pilar',
+        edad: 24
+    },
+    {
+        nombre: 'Laura',
+        edad: 24
+    },
+    {
+        nombre: 'Jenny',
+        edad: 10
+    },
+]
+
+// crea un array con la gente mayor de 25 años.
+// y muestralo por consola.
+// Sin utilizar el metodo filter de los arrays
+// Utilizad el bucle for of
+
+
+// crea un array con la gente que empieza por J
+// Sin utilizar el metodo filter de los arrays
+// Utilizad el bucle for of
+// y muestralo por consola.
+
+
+// crea un array con la gente que su nombre tiene 4 letras
+// utilizando el metodo filter de los arrays
+// y muestralo por consola.
+
+
+// crea un array con la gente que su nombre empieza por J o L
+// utilizando el metodo filter de los arrays
+// y muestralo por consola.
+
+
+
+// Rellena la matriz como la siguiente utilizando bucles for anidados
+// let matriz = [
+//   ['00','01','02'],
+//   ['10','11','12'],
+//   ['20','21','22']
+// ];
+let matriz = [
+    [],
+    [],
+    []
+]
